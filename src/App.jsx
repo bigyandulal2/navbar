285f20b04d3e0d815daae16c9b3129bb06708218
bigyandulal2 <rdravid569@gmail.com>
2025-07-15 09:52:46 +0545

    reusable navbar added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import "./App.css";
+import Hero from "./sections/Hero";
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../css/Navbar.css";
+import { IoMdArrowDropdown } from "react-icons/io";
+export default function Navbar() {
+  return (
+    <section className="header-section">
+      <h2>Bigyan</h2>
+      <nav className="header-nav">
+        <ul className="nav-items">
+          <li className="nav-item">
+            About <IoMdArrowDropdown />
+            <ul className="item-about">
+              <li>water</li>
+              <li>sugar</li>
+              <li>pappaya</li>
+              <li className="orange-items">
+                orange <IoMdArrowDropdown />
+                <ul className="orange-item">
+                  <li>Red Orange</li>
+                  <li>Green Orange</li>
+                  <li>pink Orange</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
